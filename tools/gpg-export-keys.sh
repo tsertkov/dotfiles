3b85f51a89c871c90f4941ceb40cb409670871b9
Aleksandr Tsertkov <tsertkov@gmail.com>
2016-09-05 12:08:42 +0200

    Fix gpg-export-keys

--- tools/gpg-export-keys.sh
+++ tools/gpg-export-keys.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 key=$1
-gpg2 --list-keys "$key" >/dev/null 2>&1 || echo Key \"$key\" was not found && exit 1
-gpg2 --export --armor "$key" > ./public.key
-gpg2 --export-secret-key --armon "$key" > ./private.key
+if [ gpg2 --list-keys "$key" >/dev/null 2>&1 ]; then
+  echo Key \"$key\" was not found
+  exit 1
+fi
+gpg2 --armor --export "$key" > ./public.key
+gpg2 --armor --export-secret-key "$key" > ./private.key
+chmod 600 ./private.key
