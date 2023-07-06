102cd259abba98bfb098caabc12ef205f481b446
Aleksandr Tsertkov <tsertkov@gmail.com>
2023-07-06 11:48:28 +0200

    Fix awsume-setup when awsume is not awailable

--- hooks/post-install/awsume-setup.sh
+++ hooks/post-install/awsume-setup.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+[ -x "$(which awsume-configure)" ] || exit
+
 AWSUME_SRC_SCRIPT=~/.awsume/zsh-autocomplete/_awsume
 
 [ -f "$AWSUME_SRC_SCRIPT" ] && exit
 
 echo "awsume-setup: Setting up awsume autocomplete"
-awsume-configure --shell zsh --alias-file /dev/null --autocomplete-file /dev/null 
+awsume-configure --shell zsh --alias-file /dev/null --autocomplete-file /dev/null
